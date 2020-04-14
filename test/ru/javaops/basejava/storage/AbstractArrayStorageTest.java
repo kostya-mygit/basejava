d8da67c0d2478e0f19feb8be22e913ac83b42d9c
kostya <kostya@email.com>
2020-04-14 19:03:45 +0300

    Refactor HW4

--- test/ru/javaops/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javaops/basejava/storage/AbstractArrayStorageTest.java
@@ -1,5 +1,6 @@
 package ru.javaops.basejava.storage;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.javaops.basejava.exception.ExistStorageException;
@@ -10,23 +11,27 @@ import ru.javaops.basejava.model.Resume;
 import static org.junit.jupiter.api.Assertions.*;
 import static ru.javaops.basejava.storage.AbstractArrayStorage.STORAGE_LIMIT;
 
-abstract class AbstractArrayStorageTest {
-    protected static Storage storage;
+public abstract class AbstractArrayStorageTest {
+    protected Storage storage;
 
     protected static final String UUID_1 = "uuid1";
     protected static final String UUID_2 = "uuid2";
     protected static final String UUID_3 = "uuid3";
 
-    protected static final Resume r1 = new Resume(UUID_1);
-    protected static final Resume r2 = new Resume(UUID_2);
-    protected static final Resume r3 = new Resume(UUID_3);
+    protected static final Resume RESUME_1 = new Resume(UUID_1);
+    protected static final Resume RESUME_2 = new Resume(UUID_2);
+    protected static final Resume RESUME_3 = new Resume(UUID_3);
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @BeforeEach
     public void setUp() {
         storage.clear();
-        storage.save(r1);
-        storage.save(r2);
-        storage.save(r3);
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 
     @Test
@@ -37,7 +42,7 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void saveAlreadyExist() {
-        Exception exception = assertThrows(ExistStorageException.class, () -> storage.save(r1));
+        Exception exception = assertThrows(ExistStorageException.class, () -> storage.save(RESUME_1));
         String expectedMessage = "The resume with uuid uuid1 already exists.";
         assertEquals(expectedMessage, exception.getMessage());
     }
@@ -47,13 +52,17 @@ abstract class AbstractArrayStorageTest {
         for (int i = 3; i < STORAGE_LIMIT; i++) {
             storage.save(new Resume());
         }
-        assertEquals(10000, storage.size());
+        assertEquals(STORAGE_LIMIT, storage.size());
     }
 
     @Test
     void saveStorageOverflow() {
-        for (int i = 3; i < STORAGE_LIMIT; i++) {
-            storage.save(new Resume());
+        try {
+            for (int i = 3; i < STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assertions.fail();
         }
         Exception exception = assertThrows(StorageException.class, () -> storage.save(new Resume()));
         String expectedMessage = "Unable to save the resume. The storage is full.";
@@ -64,7 +73,7 @@ abstract class AbstractArrayStorageTest {
     void update() {
         Resume r = new Resume("uuid1");
         storage.update(r);
-        assertArrayEquals(new Resume[]{r, r2, r3}, storage.getAll());
+        assertArrayEquals(new Resume[]{r, RESUME_2, RESUME_3}, storage.getAll());
     }
 
     @Test
@@ -76,7 +85,9 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void get() {
-        assertEquals(r1, storage.get(UUID_1));
+        assertEquals(RESUME_1, storage.get(UUID_1));
+        assertEquals(RESUME_2, storage.get(UUID_2));
+        assertEquals(RESUME_3, storage.get(UUID_3));
     }
 
     @Test
@@ -95,7 +106,7 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void getAll() {
-        assertArrayEquals(new Resume[]{r1, r2, r3}, storage.getAll());
+        assertArrayEquals(new Resume[]{RESUME_1, RESUME_2, RESUME_3}, storage.getAll());
     }
 
     @Test
--- test/ru/javaops/basejava/storage/ArrayStorageTest.java
+++ test/ru/javaops/basejava/storage/ArrayStorageTest.java
@@ -1,27 +1,25 @@
 package ru.javaops.basejava.storage;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.javaops.basejava.model.Resume;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ArrayStorageTest extends AbstractArrayStorageTest {
-    @BeforeAll
-    static void init() {
-        storage = new ArrayStorage();
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
     }
 
     @Test
     void save() {
         Resume r = new Resume("uuid0");
         storage.save(r);
-        assertArrayEquals(new Resume[]{r1, r2, r3, r}, storage.getAll());
+        assertArrayEquals(new Resume[]{RESUME_1, RESUME_2, RESUME_3, r}, storage.getAll());
     }
 
     @Test
     void delete() {
         storage.delete(UUID_1);
-        assertArrayEquals(new Resume[]{r3, r2}, storage.getAll());
+        assertArrayEquals(new Resume[]{RESUME_3, RESUME_2}, storage.getAll());
     }
 }
