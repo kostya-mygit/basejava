f1d340b0b7d1c52e372d02b7621479a418c4703f
kostya <kostya@email.com>
2020-06-16 11:55:36 +0300

    Add Maven

--- src/test/java/ru/javaops/basejava/storage/AbstractStorageTest.java
+++ src/test/java/ru/javaops/basejava/storage/AbstractStorageTest.java
@@ -11,7 +11,8 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public abstract class AbstractStorageTest {
     protected static final String STORAGE_DIR = "./storage";
