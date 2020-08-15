bbfe0f45e097c6f726d3097fc51e481ab6839bb9
kostya <kostya@email.com>
2020-08-15 14:29:34 +0300

    Add HW15

--- src/main/java/ru/javaops/basejava/web/ResumeServlet.java
+++ src/main/java/ru/javaops/basejava/web/ResumeServlet.java
@@ -1,10 +1,15 @@
 package ru.javaops.basejava.web;
 
+import ru.javaops.basejava.model.Resume;
+import ru.javaops.basejava.storage.SqlStorage;
+import ru.javaops.basejava.storage.Storage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -14,9 +19,28 @@ public class ResumeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        //response.setHeader("Content-type", "text/html;charset=UTF-8");
         response.setContentType("text/html;charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Guest!" : "Hello " + name + '!');
+
+        String dbUrl = getServletContext().getInitParameter("dbUrl");
+        String dbUser = getServletContext().getInitParameter("dbUser");
+        String dbPassword = getServletContext().getInitParameter("dbPassword");
+        Storage storage = new SqlStorage(dbUrl, dbUser, dbPassword);
+
+        String tableHeaders = "<table>\n" +
+                "  <tr>\n" +
+                "    <th>UUID</th>\n" +
+                "    <th>Full Name</th> \n" +
+                "  </tr>\n";
+        StringBuilder sb = new StringBuilder(tableHeaders);
+        List<Resume> resumes = storage.getAllSorted();
+        for (Resume r : resumes) {
+            sb.append("  <tr>\n" +
+                    "    <td>" + r.getUuid() + "</td>\n" +
+                    "    <td>" + r.getFullName() + "</td>\n" +
+                    "  </tr>\n");
+        }
+        sb.append("</table>");
+
+        response.getWriter().write(sb.toString());
     }
 }
